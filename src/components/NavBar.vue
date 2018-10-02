662e55e0c055c8d6d6ff8ee70ec9abcefba9d507
brankaDakic10 <branka.dakic@yahoo.com>
2018-10-02 10:45:34 +0200

    1.Task-added bootstrap,added layout with routes, NabBar and AppMovies

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+   <div>
+        <nav class="navbar navbar-expand-lg navbar-dark main-nav">
+            <router-link class="navbar-brand" :to="{name:'home'}">AppCinema</router-link>
+            <div class="navbar-nav w-100">
+                <router-link class="nav-item nav-link" :to="{name:'movies'}">Movies</router-link>
+                <div class="navbar-nav"></div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+name:"NavBar"
+}
+</script>
+
+<style scoped>
+.main-nav{
+    background-color: cornflowerblue;
+}
+</style>
